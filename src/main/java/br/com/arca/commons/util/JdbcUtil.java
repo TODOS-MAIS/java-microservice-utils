d70769c9935db342285e6a7d74b1dffbb5bd545d
Renan Felipe Ferreira <renan.ferreira@accurate.com.br>
2021-02-25 11:56:21 -0300

    feat: [#734] adding safe get methods in JdbcUtil

--- src/main/java/br/com/arca/commons/util/JdbcUtil.java
+++ src/main/java/br/com/arca/commons/util/JdbcUtil.java
@@ -2,9 +2,31 @@ package br.com.arca.commons.util;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class JdbcUtil {
-    public static Long getSafeLong(ResultSet rs, String column) throws SQLException {
+    public static LocalDateTime getSafeLocalDateTime(ResultSet rs, String column) throws SQLException {
+        var date = rs.getTimestamp(column);
+
+        if(date != null) {
+            return date.toLocalDateTime();
+        }
+
+        return null;
+    }
+
+    public static LocalDate getSafeLocalDate(ResultSet rs, String column) throws SQLException {
+        var date = rs.getDate(column);
+
+        if(date != null) {
+            return date.toLocalDate();
+        }
+
+        return null;
+    }
+
+    public static Long getSafeLong(ResultSet rs, String column) {
         try {
             var val = rs.getLong(column);
 
