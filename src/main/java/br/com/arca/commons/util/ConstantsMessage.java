7c2850629c76e4c323fb375d478fde856f25f66e
Renan Ferreira <renan.ferreira@accurate.com.br>
2021-06-11 14:52:19 -0300

    Revert "Merge branch 'feature/1132' into 'develop'"
    This reverts merge request !54

--- src/main/java/br/com/arca/commons/util/ConstantsMessage.java
+++ src/main/java/br/com/arca/commons/util/ConstantsMessage.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public enum ConstantsMessage {
-    INVALID_ENUM("Não foi possível converter o valor %s"),
-    ORDER_LIST_INVALID_FILTER("É obrigatório preencher ao menos um campo do filtro"),
-    ORDER_LIST_INITIAL_DATE_GREATER_THAN_END_DATE("Data inicial deve ser maior que a data final"),
+    INVALID_ENUM("Não foi possível converter o valor %s")
     ;
 
     private String description;
