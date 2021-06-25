67f3a4177bfb4614fdc52c81aff8b7e497766ede
Gustavo <gustavo.souza@accurate.com.br>
2021-06-25 10:29:30 -0300

    Feature: [#1509] Adjustments for ConfirmPhone API

---
+++ src/main/java/br/com/arca/commons/util/LoginType.java
@@ -0,0 +1,23 @@
+package br.com.arca.commons.util;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum LoginType {
+    CPF,
+    TELEFONE,
+    EMAIL,
+    OUTRO,
+    FACEBOOK;
+
+    @JsonValue
+    @Override
+    public String toString() {
+        return name();
+    }
+    
+    public boolean equals(String profile) {
+        return profile.equals(this.toString());
+    }
+}
