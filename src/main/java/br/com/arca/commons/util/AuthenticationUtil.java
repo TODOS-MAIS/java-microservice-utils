983c85e90fd8c19faf52bea5ceb000848933e3b3
Renan Felipe Ferreira <renanfelipeferreira@live.com>
2021-08-19 15:56:08 -0300

    feat: [#1707] adding util classes

--- src/main/java/br/com/arca/commons/util/AuthenticationUtil.java
+++ src/main/java/br/com/arca/commons/util/AuthenticationUtil.java
@@ -1,5 +1,8 @@
 package br.com.arca.commons.util;
 
+import br.com.arca.commons.exception.DefaultException;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -8,7 +11,10 @@ import java.security.Principal;
 import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class AuthenticationUtil {
+    private final JwtUtil jwtUtil;
+
     public String getAuthenticatedUser() {
         return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication()).map(Principal::getName).orElse(null);
     }
--- src/main/java/br/com/arca/commons/util/ConstantsMessage.java
+++ src/main/java/br/com/arca/commons/util/ConstantsMessage.java
@@ -4,7 +4,8 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public enum ConstantsMessage {
-    INVALID_ENUM("Não foi possível converter o valor %s")
+    INVALID_ENUM("Não foi possível converter o valor %s"),
+    FORBIDDEN_ACCESS("Você não está autorizado a realizar essa operação")
     ;
 
     private String description;
