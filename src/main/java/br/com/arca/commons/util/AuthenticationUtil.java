0bc1f505f8a7d636679c17c08ac8bdf56e5df829
Renan Felipe Ferreira <renanfelipeferreira@live.com>
2021-08-19 17:02:16 -0300

    feat: [#1707] removing unused imports

--- src/main/java/br/com/arca/commons/util/AuthenticationUtil.java
+++ src/main/java/br/com/arca/commons/util/AuthenticationUtil.java
@@ -1,10 +1,6 @@
 package br.com.arca.commons.util;
 
-import br.com.arca.commons.exception.DefaultException;
-import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.security.Principal;
